7a0ca1d3899681adff98a6832b39cb5f0a23f797
Pablo Jácome A <pablojacomea@gmail.com>
2016-04-13 22:11:48 -0500

    Conexion a base de datos

--- src/main/java/com/udla/siscoudla/HelloServlet.java
+++ src/main/java/com/udla/siscoudla/HelloServlet.java
@@ -1,18 +1,66 @@
 package com.udla.siscoudla;
 
+import com.google.appengine.api.utils.SystemProperty;
+
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.http.*;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@SuppressWarnings("serial")
 public class HelloServlet extends HttpServlet {
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
-  @Override
-  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    PrintWriter out = resp.getWriter();
-    out.println("Hello, world");
+public void doGet(HttpServletRequest req, HttpServletResponse res)
+      throws IOException {
+    res.setContentType("text/plain");
+
+    Map<String, String> properties = new HashMap();
+    if (SystemProperty.environment.value() ==
+          SystemProperty.Environment.Value.Production) {
+      properties.put("javax.persistence.jdbc.driver",
+          "com.mysql.jdbc.GoogleDriver");
+      properties.put("javax.persistence.jdbc.url",
+          System.getProperty("cloudsql.url"));
+    } else {
+      properties.put("javax.persistence.jdbc.driver",
+          "com.mysql.jdbc.Driver");
+      properties.put("javax.persistence.jdbc.url",
+          System.getProperty("cloudsql.url.dev"));
+    }
+
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory(
+        "Demo", properties);
+
+    // Insert a few rows.
+    EntityManager em = emf.createEntityManager();
+    em.getTransaction().begin();
+    em.persist(new Greeting("user", new Date(), "Hello!"));
+    em.persist(new Greeting("user", new Date(), "Hi!"));
+    em.getTransaction().commit();
+    em.close();
+
+    // List all the rows.
+    em = emf.createEntityManager();
+    em.getTransaction().begin();
+    List<Greeting> result = em
+        .createQuery("SELECT g FROM Greeting g")
+        .getResultList();
+    for (Greeting g : result) {
+      res.getWriter().println(
+          g.getId() + " " +
+          g.getAuthor() + "(" + g.getDate() + "): " +
+          g.getContent());
+    }
+    em.getTransaction().commit();
+    em.close();
+
   }
 }
